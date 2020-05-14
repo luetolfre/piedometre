328b6f522fe792a42cd4e6b6d963d931e19976b0
Adrian <adrian.dorier@students.ffhs.ch>
2020-05-14 16:37:20 +0200

    Buttons ProgressBar 16:00

--- app/src/main/java/net/ictcampus/piedometre/MainActivity.java
+++ app/src/main/java/net/ictcampus/piedometre/MainActivity.java
@@ -3,9 +3,15 @@ package net.ictcampus.piedometre;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +20,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ConstraintLayout main = findViewById(R.id.main);
-        main.setVisibility(View.VISIBLE);
-
     }
+
+
+    public ProgressBar getProgressBarSteps() {
+        ProgressBar progressBarSteps = findViewById(R.id.progressbarSteps);
+        return progressBarSteps;
+    }
+
+    public void onClick(View view) {
+        System.out.println("clikc");
+    }
+
+
+    /*
+    private void setOnItemListener(ProgressBar progressBar) {
+        AdapterView.OnItemClickListener onListClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), .class);
+                String selected = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT).show();
+                intent.putExtra("kanton", selected);
+                startActivity(intent);
+            }
+        };
+
+        progressBar.setOnItemClickListener(onListClickListener);
+    }*/
 }
