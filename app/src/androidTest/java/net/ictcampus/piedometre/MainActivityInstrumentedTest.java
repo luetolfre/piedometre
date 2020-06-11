6429cc288d566fae8470f49cb644ff17719a429c
Luetolf Rebekka <luetolfre@ict-campus.net>
2020-06-11 17:56:41 +0200

    add test class

---
+++ app/src/androidTest/java/net/ictcampus/piedometre/MainActivityInstrumentedTest.java
@@ -0,0 +1,20 @@
+package net.ictcampus.piedometre;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityInstrumentedTest {
+
+    @Test
+    public void startTrainingActivity() {
+    }
+
+    @Test
+    public void startStatsActivity() {
+    }
+
+    @Test
+    public void startProfileActivity() {
+    }
+}
